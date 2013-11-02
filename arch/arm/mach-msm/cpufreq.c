09b4eac8b2d71fe5c0b71f27315c56c1d401264a
Paul Reioux <reioux@gmail.com>
2013-11-02 18:38:31 -0500

    cpufreq: intellidemand governor (v5.5)
    cpufreq/cpufreq_intellidemand: intellidemand cpufreq governor (version 4.0)
    
    Intellidemand is inspired by ondemand governor. Its main features are:
    
     - auto cpu hotplugging based on runnable thread average calculations
     - detection of browsing mode (long screen on activities with moderate run
       queue depth =< 45 )
     - limit max frequencies when in browsing mode to conserve battery for long
       duration tasks
    
    Browsing State Determination:
     1- Browsing state is enabled as power-on default. Browsing state is disabled
        when run queue depth is > 45 (very high instantaneous spikes)
    
    Browsing State Limit Max Freq Logic (sliding window algorithm):
     1- While Screen is ON > 3 min, and average load of the CPU is < 35%, then
        CPU Max frequencies will remain power-on default
     2- While Screen is On, if average CPU load > 35%, both CPUs max frequencies
        will be limited to a lower user configurable max value
     3- While Screen is On, After both CPUs' Max is limited to lowered max, when
        the average CPU load has dropped down to less than < 25%, then both CPUs
        Max freq will be restored to back to power-on default
    
    Runnable Thread Auto-hotplug (fast hotplug / slow unplug algorithm):
     1- When average number of running thread is < threshold, 2nd cpu will be
        unplugged.
     2- When > threshold, 2nd cpu will be hotplugged.
     3- The logic applies a fast hotplug and slow unplug algorithm so 2nd core
        will stay enabled longer to help deal with performance spikes
    
    changes from Ver 2.0 -> Ver 3.0:
      - remove GPU Busy dependency for browsing state detection (this will make
        the governor more portable to other platforms without specific GPU
        architecture knowledge)
    
    dependencies:
      -- Qualcomm's run queue average logic (SOC independent logic)
      -- TI's runnable thread average logic (SOC independent logic)
    
    forward ported to Linux 3.4 for use on S-800
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Conflicts:
    	arch/arm/mach-msm/cpufreq.c
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>
    
    intellidemand: code derps clean up!
    
    Signed-off-by: faux123 <reioux@gmail.com>
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>
    
    Intellidemand: put cores 1~3 to sleep while screen is off
    
    Signed-off-by: faux123 <reioux@gmail.com>
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>
    
    arch/arm/mach-msm/cpufreq: remove deprecated logic
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>
    
    intellidemand: allow clean compilation when LMF is disabled
    
    bumped version to 4.2
    
    Signed-off-by: faux123 <reioux@gmail.com>
    
    Conflicts:
    	arch/arm/mach-msm/cpufreq.c
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>
    
    arch/arm/mach-msm/cpufreq.c: remove legacy intellidemand leftovers
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>
    
    Fixes and bump intellidemand to 5.5
    
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/Makefile
    
    Change-Id: I326b4aea5d1e8c481b9a26b80fe2a692433efdf3

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -308,6 +308,7 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 
 static int __cpuinit msm_cpufreq_cpu_callback(struct notifier_block *nfb,
 		unsigned long action, void *hcpu)
+
 {
 	unsigned int cpu = (unsigned long)hcpu;
 	int rc;
