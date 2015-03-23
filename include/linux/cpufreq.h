904605eab2fa90268b7de0a63e70f6ed210f707b
Arnav Gosain <gosain.arnav@gmail.com>
2015-03-23 17:11:37 -0400

    cpufreq: add optimax governor
    Didn't know who the original author is so couldn't retain authorship since I took it from another kernel but credits to him/her!
    
    Change-Id: I143e05f4f17a4b8c117343f24eb4d46f2ce1d2c0

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -382,6 +382,9 @@ extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKACTIVE)
 extern struct cpufreq_governor cpufreq_gov_yankactive;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_yankactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_OPTIMAX)
+extern struct cpufreq_governor cpufreq_gov_optimax;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_optimax)
 #endif
 
 
